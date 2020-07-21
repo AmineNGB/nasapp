a8a423a92a0dc27501abb22eb3ea8d139efef3a9
amineneghbel <amineneghbel@gmail.com>
2020-07-21 18:04:24 +0200

    Updates

---
+++ precache-manifest.dfce4b76723a664d5628ea1bf716bde3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635584d2d88c4ff7cbf96401347d4a5f",
+    "url": "/nasapp/index.html"
+  },
+  {
+    "revision": "a4c7255672bf5a80bf85",
+    "url": "/nasapp/static/css/main.d01c4391.chunk.css"
+  },
+  {
+    "revision": "929490296c7564c6cecc",
+    "url": "/nasapp/static/js/2.ff695738.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nasapp/static/js/2.ff695738.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c7255672bf5a80bf85",
+    "url": "/nasapp/static/js/main.4b0c1d67.chunk.js"
+  },
+  {
+    "revision": "a7e19c551e4d21a6acb1",
+    "url": "/nasapp/static/js/runtime-main.68f4552f.js"
+  },
+  {
+    "revision": "5df9840df4363b73ce1cdce2cf951683",
+    "url": "/nasapp/static/media/bg.5df9840d.jpg"
+  },
+  {
+    "revision": "d561442c9d788f010569cc48b6227692",
+    "url": "/nasapp/static/media/moon.d561442c.png"
+  },
+  {
+    "revision": "ecd840aa425aa8748956982e47f0b342",
+    "url": "/nasapp/static/media/mountain.ecd840aa.png"
+  },
+  {
+    "revision": "d9d4d4140463ac79b4a2511ef27b511a",
+    "url": "/nasapp/static/media/road.d9d4d414.png"
+  }
+]);
