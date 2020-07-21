f0a20e807427f30fb2e0781e2115541319111b26
amineneghbel <amineneghbel@gmail.com>
2020-07-21 20:51:09 +0200

    hash route setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { HashRouter, Switch, Route } from 'react-router-dom'
 import Home from "./components/Home"
 import NasaPhoto from "./components/NasaPhoto"
+import NotFound from "./components/NotFound"
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app">
-        <Route component={Home} path="/" exact />
-        <Route component={NasaPhoto} path="/nasaphoto" exact />
-      </div>
-    </BrowserRouter>
+    <HashRouter>
+        <Switch>
+        <div className="app">
+          <Route component={Home} path="/" exact />
+          <Route component={NasaPhoto} path="/nasaphoto" exact /> 
+        </div>  
+        <Route component={NotFound} />
+        </Switch>
+      </HashRouter>
   );
 }
 
---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <img src="https://freefrontend.com/assets/img/html-css-404-page-templates/404-Not-Found-SVG.png" alt="404"/>
+        </div>
+    );
+};
+
+export default NotFound;
